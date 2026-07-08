f93a156aad75c475b1dc0835ab895bcb57fd1c56
frontsmartdev-glitch <frontsmartdev@gmail.com>
2026-07-08 18:19:44 +0100

    add new functional confirm,alert,console.log, if-else, buttons change colors

--- main.js
+++ main.js
@@ -10,9 +10,7 @@ changeAllCardColorButton.addEventListener('click', () => {
     });
     });
 
-
-console.log('productCards', productCards)
-// console.log('changeCardColorButton', changeCardColorButton)
+// Покраска первой карточки
 
 const firstProductCard = document.querySelector('.product-card');
 const changeFirstCardColorButton = document.querySelector('#change-color-first-card');
@@ -20,3 +18,31 @@ const changeFirstCardColorButton = document.querySelector('#change-color-first-c
 changeFirstCardColorButton.addEventListener('click', () => {
     firstProductCard.style.backgroundColor = blueColorHash;
 });
+
+// Открытие Google в новой вкладке
+
+const openGoogleButton = document.querySelector('#open-google');
+
+openGoogleButton.addEventListener('click', openGoogle  
+,);
+
+function openGoogle() {
+    const confirmOpen = confirm('Вы уверены, что хотите открыть Google?');
+    
+    if (confirmOpen === true) {
+        window.open('https://www.google.com')
+    } else {
+        return;
+    }
+}
+
+// Вывод в консоль лог
+
+const outputToConsoleLogButton = document.querySelector('#output-to-console-log');
+
+outputToConsoleLogButton.addEventListener('click', () => outputToConsoleLog('Homework: No6'));
+    
+function outputToConsoleLog(message) {
+    alert(message);
+    console.log(message);
+}
