45bac8069a5da3838c3520db0e028c6b38771665
frontsmartdev-glitch <frontsmartdev@gmail.com>
2026-08-03 05:48:30 +0100

    feat add function speedofLight, buy product, const let var

--- homework-7.js
+++ homework-7.js
@@ -1,6 +1,7 @@
 function showTemperature(city1, temperature2) { 
     console.log(`Сейчас в ${city1} температура —${temperature2}  градусов`);
-} 
+    } 
+
 showTemperature('Istanbul', +20);
 
 const speedOfLight = 299792458;
@@ -17,3 +18,26 @@ const speedOfLight = 299792458;
 checkSpeed(500000000);  
 checkSpeed(150000000);     
 checkSpeed(299792458);      
+
+const product = 'laptop';
+const price = 1500;
+
+    function checkBudget(budget) {
+        if (budget > price) {
+            console.log(`${product} приобретён. Спасибо за покупку!`);
+        } else {
+            console.log(`Вам не хватает ${price - budget}$, пополните баланс`);
+        }
+}
+
+checkBudget(2000);
+checkBudget(1000);
+
+function showResult(footballer, time) {
+    console.log(`${footballer} забил за ${time} секунд`);
+}
+showResult('Ronaldo', 58);
+
+const country = Crimea;
+let number = 6;
+var isIsland = true;
